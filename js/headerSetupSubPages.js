db051a385c74f0b94fdef4fa5bdeccc91f14d697
Viktor Hristov <hristov_98@yahoo.com>
2021-01-26 21:17:22 +0200

    Add JS to set main page and navbar button links

---
+++ js/headerSetupSubPages.js
@@ -0,0 +1,17 @@
+function addLinkToButton(buttonId, pageURL) {
+    document.getElementById(buttonId).addEventListener("click", function () {
+        window.location = pageURL;
+    });
+}
+
+addLinkToButton("mainPageButton", "../../index.html");
+
+addLinkToButton("invertebratesButton", "../../html/animalSections/invertebrates.html");
+addLinkToButton("crustaceansButton", "../../html/animalSections/crustaceans.html");
+addLinkToButton("fishButton", "../../html/animalSections/fish.html");
+addLinkToButton("sharksButton", "../../html/animalSections/sharks.html");
+addLinkToButton("reptilesButton", "../../html/animalSections/reptiles.html");
+addLinkToButton("mammalsButton", "../../html/animalSections/mammals.html");
+
+addLinkToButton("loginButton", "../../html/userControl/login.html");
+addLinkToButton("registerButton", "../../html/userControl/registration.html");
