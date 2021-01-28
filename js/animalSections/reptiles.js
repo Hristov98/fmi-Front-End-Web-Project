e294c662d6c0aefb311a48c4ff3a78d3b117a3b7
Viktor Hristov <hristov_98@yahoo.com>
2021-01-28 21:48:53 +0200

    Add reptile section entries

---
+++ js/animalSections/reptiles.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let jsonResultArray;
+        jsonResultArray = JSON.parse(this.responseText);
+
+        for (let i = 0; i < jsonResultArray.length; i++) {
+            if (jsonResultArray[i].type === "Reptile") {
+                let anchorHTML = createEntryAnchor(jsonResultArray[i].id, jsonResultArray[i].name);
+                addAnchorToPage(anchorHTML);
+
+                let entryHTML = createAnimalEntry(jsonResultArray[i]);
+                addAnimalEntryToPage(entryHTML);
+            }
+        }
+    }
+};
+
+let jsonURL = "http://127.0.0.1:5500/js/animalSections/animalInformation.json";
+xhttp.open("GET", jsonURL, true);
+xhttp.send();
+
+document.getElementById("reptilesButton").style.color = "white";
+document.getElementById("reptilesButton").style.backgroundColor = "#2c328bbe";
+document.getElementById("reptilesButton").style.border = "0.1rem solid white";
